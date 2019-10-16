815a9da1676e9273bd3f879b6291df6d393ab8b3
Liz P <parisielizabeth@gmail.com>
2019-10-16 18:51:56 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
-    t.string "Last_name"
+    t.string "last_name"
   end
 
 end
